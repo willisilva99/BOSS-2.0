1e1954e75031b9bb9ba7e9aff482da1cbfe1af00
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2024-10-30 16:44:29 -0300

    Update rank.py

--- rank.py
+++ rank.py
@@ -9,20 +9,21 @@ CARGOS = {
 class RankManager:
     @staticmethod
     async def update_rankings(bot):
-        guild = bot.get_guild(1186390028990025820)  # Guild ID do seu servidor
+        guild = bot.get_guild(1186390028990025820)  # Insira o Guild ID do servidor
         top_players = await DatabaseManager.get_top_players()
 
         for i, (user_id, damage) in enumerate(top_players):
             member = guild.get_member(user_id)
-            cargo_id = CARGOS[i + 1]
+            if member:
+                cargo_id = CARGOS[i + 1]
 
-            # Remove cargos de jogadores fora do top 3
-            for cargo in CARGOS.values():
-                role = guild.get_role(cargo)
-                if role in member.roles and cargo != cargo_id:
-                    await member.remove_roles(role)
+                # Remove cargos de jogadores fora do top 3
+                for cargo in CARGOS.values():
+                    role = guild.get_role(cargo)
+                    if role in member.roles and cargo != cargo_id:
+                        await member.remove_roles(role)
 
-            # Adiciona cargo ao top player da posição correta
-            top_role = guild.get_role(cargo_id)
-            if top_role not in member.roles:
-                await member.add_roles(top_role)
+                # Adiciona cargo ao top player da posição correta
+                top_role = guild.get_role(cargo_id)
+                if top_role not in member.roles:
+                    await member.add_roles(top_role)
